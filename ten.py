5d423030f3ae30d36921d56a89b82d6136856f42
charlottecode <charlottedesigner2013@gmail.com>
2025-02-12 20:05:17 -0500

    Add files via upload

---
+++ ten.py
@@ -0,0 +1,11 @@
+print("hello my name is charlotte")
+
+#print my favorite animal is the dogs
+
+print("my favorite animal is  the dogs")
+
+#print 8
+print(8)
+
+#sprint and number
+print("12",13)
---
+++ variable.py
@@ -0,0 +1,9 @@
+a=3
+b=2
+
+print(a)
+print(b)
+
+name=input("Enter your name")
+
+print("nice to meet you",name)
